0396ad2d098c428786d90df69cf8a7e66f7dd301
Edoardo Ruffoli <ruffoli99@gmail.com>
2022-06-23 18:23:42 +0200

    Create test1.py

---
+++ mininet/test1.py
@@ -0,0 +1,64 @@
+import requests
+import json
+import time
+from mininet.log import setLogLevel, info
+from mininet.util import pmonitor
+from signal import SIGINT
+
+def configure_test() :
+	controller_ip = "127.0.0.1:8080"
+	base_url = "http://" + controller_ip + "/qos/"
+	header = {"Content-type": "application/json", "Accept": "text/plain"}
+
+	url_switches = base_url + "switches/json"
+	url_flows = base_url + "flows/json"
+
+	# Cleaning previous config
+	print(requests.delete(url_switches, data=json.dumps(
+		{"dpid-meter-switch": "00:00:00:00:00:00:00:06", 
+		 "dpid-queue-switch": "00:00:00:00:00:00:00:07"}
+		), headers=header))
+
+	# Enabling Switches
+	print(requests.post(url_switches, data=json.dumps(
+		{"dpid-meter-switch": "00:00:00:00:00:00:00:06", 
+		 "dpid-queue-switch": "00:00:00:00:00:00:00:07"}
+		), headers=header))
+
+	# Registering QoS traffic flows
+	print(requests.post(url_flows, data=json.dumps(
+		{"dpid-meter-switch": "00:00:00:00:00:00:00:06", 
+		 "dpid-queue-switch": "00:00:00:00:00:00:00:07",
+		 "src-addr": "10.0.0.2",
+		 "dst-addr": "10.0.0.5",
+		 "bandwidth": "5000"}
+		), headers=header))
+
+def run_test(net):
+	h1 = net.getNodeByName('h1')
+	h2 = net.getNodeByName('h2')
+	h5 = net.getNodeByName('h5')
+
+	# Configuring Flows
+	info("*** Configuring QoS Traffic Flows\n")
+	configure_test()
+
+	# Iperf tests
+	info("*** Starting Test 1\n")
+	endTime = time.time() + 35
+
+	info("*** Started iperf server on h5\n")
+	h5.cmd("xterm -T h5 -l -lf output/test1.txt -hold -e iperf -s -i 1 &")
+	time.sleep(3)
+
+	info("*** Started iperf client on h1 to saturate link\n")
+	h1.cmd("xterm -T h1 -hold -e iperf -c 10.0.0.5 -p 5001 -b 10M -t 30 &")
+	time.sleep(5)
+
+	info("*** Started iperf client on h2\n")
+	h2.cmd("xterm -T h2 -hold -e iperf -c 10.0.0.5 -p 5001 -b 5M -t 25 &")
+	time.sleep(5)
+
+if __name__ == '__main__':
+	configure_test()
+
