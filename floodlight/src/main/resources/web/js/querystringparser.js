f0a956aedc6ccee9296e25bb8966f41d8e984e59
edoardoruffoli <ruffoli99@gmail.com>
2022-06-27 14:04:43 -0400

    final push

--- floodlight/src/main/resources/web/js/querystringparser.js
+++
@@ -1,8 +0,0 @@
-﻿function getQueryParameterByName(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-
-var controllerIp = getQueryParameterByName("cip");
